4ccb1857e7c42017e8f4fa7f77193eef1c2e5e1d
intelligentzombie1 <kayodeoshodi400@gmail.com>
2020-03-19 14:33:44 +0100

    Updates

---
+++ precache-manifest.767f2ff34e4f6aa90913d0dac5a87192.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe3cbb6123360d33024d440758404a6",
+    "url": "/react-meme-generaator/index.html"
+  },
+  {
+    "revision": "20938db51b383b3a282d",
+    "url": "/react-meme-generaator/static/css/main.049db5c9.chunk.css"
+  },
+  {
+    "revision": "7274b1c6500d07f6d11e",
+    "url": "/react-meme-generaator/static/js/2.3fa4bed5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-meme-generaator/static/js/2.3fa4bed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20938db51b383b3a282d",
+    "url": "/react-meme-generaator/static/js/main.befbe71f.chunk.js"
+  },
+  {
+    "revision": "f749111472bca6531590",
+    "url": "/react-meme-generaator/static/js/runtime-main.0acb35bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-meme-generaator/static/media/logo.5d5d9eef.svg"
+  }
+]);
